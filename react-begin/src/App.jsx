af9b6a3dc059c10a8e7ab01cc72511a6a1b55f8f
diksha <rajdiksha91@gmail.com>
2024-03-06 16:40:52 +0530

    props

--- react-begin/src/App.jsx
+++ react-begin/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import Hello  from "./components/Hello"
+import { useState } from "react";
+import Hello from "./components/Hello";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Hello/>
+      <Hello message="Hey there!" name="Diksha" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-begin/src/components/Hello.jsx
+++ react-begin/src/components/Hello.jsx
@@ -1,5 +1,12 @@
-function Hello(){
-    return <h1>Hello from a component</h1>;
+function Hello(props) {
+  return (
+    <>
+      <h1>
+        {" "}
+        {props.message} {props.name}{" "}
+      </h1>
+    </>
+  );
 }
 
 export default Hello;
